cbe7f0c6a73a1a7fd220d051ecf3975b67ff310c
yasir-8arafat <arafat23105131008@diu.edu.bd>
2026-03-08 14:46:42 +0600

    Initilize React Countries Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Countries from './Components/Countries/Countries'
+
+function App() {
+  return (
+    <>
+      <h1>Countries React</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
